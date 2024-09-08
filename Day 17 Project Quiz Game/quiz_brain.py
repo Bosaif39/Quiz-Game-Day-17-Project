f809c91bc9bb33b8c27db237c2e9ae4af1e43718
Abdullah Saif <bosaif39tm@gmail.com>
2024-09-09 00:34:09 +0400

    Update quiz_brain.py

--- Day 17 Project Quiz Game/quiz_brain.py
+++ Day 17 Project Quiz Game/quiz_brain.py
@@ -1,30 +1,26 @@
-
 class QuizBrain:
-    def __init__(self,qList):
-        self.questionNum=0
-        self.questionList=qList
-        self.score=0
-    #Method  to check is there still more question 
-    def still_has_question(self):
-        return self.questionNum<len(self.questionList)
-    
-    #Method for next question
-    def next_question(self):
-        current_question=self.questionList[self.questionNum]
-        self.questionNum +=1
-	    
-        user_ans=input(f"Q.{self.questionNum}: {current_question.text} (True/False):")
-        self.checker(user_ans,current_question.answer)
-    
-    #Method to check correct answer
-    def checker(self,user_ans,correct_ans):
-        if(user_ans.lower() ==correct_ans.lower()):
-            self.score+=1
-            print("Correct ans")
-        else:
-            print("Incorrect ans")
-        
-	#Feedback for the user
-        print(f"The correct ans is {correct_ans}")
-        print(f"The current score is {self.score} / {self.questionNum} \n")
+    def __init__(self, qList):
+        self.questionNum = 0  # Tracks the current question number
+        self.questionList = qList  # A list of questions passed to the quiz
+        self.score = 0  # Tracks the user's score
 
+    def still_has_question(self):
+        return self.questionNum < len(self.questionList)
+
+    def next_question(self):
+        current_question = self.questionList[self.questionNum]
+        self.questionNum += 1
+
+        user_ans = input(f"Q.{self.questionNum}: {current_question.text} (True/False): ")
+        self.checker(user_ans, current_question.answer)
+
+    def checker(self, user_ans, correct_ans):
+        if user_ans.lower() == correct_ans.lower():
+            self.score += 1
+            print("Correct answer!")
+        else:
+            print("Incorrect answer.")
+
+        # Provide feedback on the correct answer and current score
+        print(f"The correct answer is: {correct_ans}")
+        print(f"Your current score is {self.score} / {self.questionNum}\n")
